f9932b402a68ae548a355b1c48c2b3b318d2ca2e
mumair <mumairofficial@gmail.com>
2018-10-05 23:10:06 +0500

    another preview

--- rahimia-library/src/views/Home.vue
+++ rahimia-library/src/views/Home.vue
@@ -2,6 +2,9 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App Umair good to see you"/>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perferendis, fugiat? Consequatur nemo officiis magnam facere! Beatae suscipit cum nam molestias eos libero illum magnam deserunt nobis! Officia esse dolore qui?
+    </p>
   </div>
 </template>
 
@@ -12,7 +15,7 @@ import HelloWorld from '@/components/HelloWorld.vue';
 export default {
   name: 'home',
   components: {
-    HelloWorld,
-  },
+    HelloWorld
+  }
 };
 </script>
