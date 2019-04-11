7e04762f5cf5844c55f66fa60d770dc6e854f162
mumair <mumairofficial@gmail.com>
2019-04-11 11:46:10 +0500

    Workspace cleanup

--- rahimia-library/src/components/BasicLayout.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div class="container mx-auto lg:w-full pt-6">
-        <slot></slot>
-    </div>
-</template>
--- rahimia-library/src/components/tag.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div class="`text-xs px-4 py-2 rounded-full`" v-bind="{}">{{tag}}</div>
-</template>
-
-<script>
-export default {
-  props: ['text', 'color'],
-};
-</script>
